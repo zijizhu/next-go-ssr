ae4e06a1064f173be607822beb8675385a9e0318
Zi-ji <zhuzhijie1003@outlook.com>
2022-03-07 17:57:39 +1100

    added README.md

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
-    "http://localhost:3000",
+    "http://localhost:3001",
     "http://localhost:8000",
 ]
 
