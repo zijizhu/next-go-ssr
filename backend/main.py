46380d8d48a117c0b36f677cb2604fbd6d791727
Zi-ji <zhuzhijie1003@outlook.com>
2022-03-07 17:28:48 +1100

    ssr complete

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get('/blog/count')
+def get_blog_1():
+    return {"count": "2"}
 
 
 @app.get('/blog/1')
